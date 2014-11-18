f6827fa05822f420f2a9a1a3ff8d1bd205f6db5e
Rishi Ramraj <rishi@bestpremise.com>
2014-11-18 06:44:44 -0500

    Added a proper solution for fibbonacci.

--- solutions/algorithms/fibbonacci.py
+++ solutions/algorithms/fibbonacci.py
@@ -5,17 +5,10 @@ Generate the nth Fibonacci number without using recursion.
 
 
 def fibonacci(n):
-    # Exit early.
-    if n < 3:
-        return 1
-
-    # Iteratively build a cache.
-    cache = [1, 1]
-    for index in range(1, n-1):
-        cache.append(cache[index] + cache[index-1])
-
-    # Return the result.
-    return cache[-1]
+    result = [1, 1]
+    for index in range(2, n):
+        result.append(result[index-1] + result[index-2])
+    return result[-1]
 
 
 test = 8
