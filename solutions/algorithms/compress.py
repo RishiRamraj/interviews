deb52710774f61a22a95a7c811f9290ac743b2a4
Rishi Ramraj <rishi@bestpremise.com>
2016-10-30 18:05:33 -0400

    A better solution for compress

--- solutions/algorithms/compress.py
+++ solutions/algorithms/compress.py
@@ -3,34 +3,21 @@ Problem:
 Compress a string.
 '''
 
-def compress(string):
-    encoding = []
-    value = None
-    count = 0
 
-    # Build the run length encoding.
-    for index, next in enumerate(string):
-        # Check for a change.
-        if value != next:
-            # Check for a new encoding.
-            if count:
-                encoding.append((value, count))
+def compress(result, next):
+    # Get last and count.
+    count = int(result[-1]) if result[-1].isdigit() else 1
+    last = result[-1] if count == 1 else result[-2]
 
-            # Switch to the new character.
-            value = next 
-            count = 0
+    # Update the encoding.
+    if next == last:
+        index = None if count == 1 else -1
+        result = result[:index] + str(count+1)
 
-        # Increment the count.
-        count += 1
-
-    # Add the last character.
-    encoding.append((value, count))
-
-    # Render the encoding.
-    result = ''
-    for value, count in encoding:
-        result += '%d%s' % (count, value) if count > 1 else value
+    else:
+        result += next
 
     return result
 
-print(compress('mississippi'))
+
+print(reduce(compress, 'mississippi'))
